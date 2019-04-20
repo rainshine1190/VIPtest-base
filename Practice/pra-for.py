d9a2746de90f1b7172be2d14185a8c1526b45e05
rainshine1190 <rainshine1190@126.com>
2019-04-20 20:35:18 +0800

    初始化项目，添加循环，分支，数据结构等代码

---
+++ Practice/pra-for.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+# __author__ = 'lc'
+
+#语法
+# for i in [1,2,1,4]:
+#     print(i,'ok')
+
+
+#range
+for i in range(1,6):
+    if i == 3:
+        continue
+    print(i)
--- Practice/pra-while.py
+++ Practice/pra-while.py
@@ -1,10 +1,11 @@
 #coding:utf-8
+
 # __author__ = 'lc'
 
+# n = 5
 #
-# try:
+# while n > 0:
 #     a = int(input('please input the num:'))
-#
 #     if a > 0:
 #         a += 10
 #     elif a == 0:
@@ -13,10 +14,27 @@
 #         a += 30
 #     print(a)
 #
+#     n = n -1
+
+
+#练习题
+
+
+
+# sum = 0
 #
-# except ValueError:
-#     print('请输入数字！')
+# n = 1
+#
+# while n < 101:
+#     sum = sum + n
+#     n = n + 1
+#
+# print(sum)
 
 
 
-print(range(1,10))
+
+
+
+
+
