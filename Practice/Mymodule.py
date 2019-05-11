a5043d0b9473d336c82a8ba0e22a07a14e164eec
rainshine1190 <rainshine1190@126.com>
2019-05-11 16:18:10 +0800

    基础和高级

---
+++ Practice/Mymodule.py
@@ -0,0 +1,7 @@
+#coding:utf-8
+# __author__ = 'lc'
+
+def fun():
+    print('我是fun函数')
+
+
---
+++ 面向对象/02-继承重写.py
@@ -0,0 +1,33 @@
+#coding:utf-8
+# __author__ = 'lc'
+
+
+class Animal:
+    def eat(self):
+        print('---------吃')
+    def drink(self):
+        print('---------喝')
+
+
+class Dog(Animal):
+    # def eat(self):
+    #     print('---------吃')
+    # def drink(self):
+    #     print('---------喝')
+    def bark(self):
+        print('---------汪汪叫')
+
+
+class Xiaotq(Dog):
+    def fly(self):
+        print('------飞')
+
+    def bark(self):
+        print('-------哮天犬狂叫')
+
+
+x = Xiaotq()
+x.fly()
+x.bark()
+#子类可以拥有父类的父类的功能
+x.eat()
